e2dcb8aa165fc1f4cf81e6e34c5bd9a8c5f6cdeb
Mary Bucknell <mbucknell@usgs.gov>
2015-08-05 15:49:14 -0500

    Added jsontemplate as a requirement and using this simple templating language to convert the received json to a sitemap.xml file.

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -1,5 +1,20 @@
 import argparse
 
+import jsontemplate
+import requests
+
+def get_feature(geoserver_endpoint, layer, attribute):
+    response = requests.get(geoserver_endpoint + 'wfs', 
+                            {'service' : 'wfs',
+                             'version' : '2.0.0',
+                             'request' : 'GetFeature',
+                             'typeNames' : layer,
+                             'propertyName' : attribute,
+                             'outputFormat' : 'json'
+                             })
+    response.raise_for_status()
+    return response.json()
+
 if __name__=="__main__":
     
     parser = argparse.ArgumentParser(description='Generate sitemap.xml for NWC')
@@ -7,4 +22,22 @@ if __name__=="__main__":
                         default='http://cida.usgs.gov/nwc/geoserver/')
     args = parser.parse_args()
     
+    template = open('sitemap.template', 'r');
+    sitemap_file = open('sitemap.xml', 'w');
+    
     print 'Retrieving HUCs and gage IDs from %s' % args.geoserver
+    
+    waterbudget_hucs = get_feature(args.geoserver, 'NHDPlusHUCs:nationalwbdsnapshot', 'huc_12')    
+    streamflow_gage_ids = get_feature(args.geoserver, 'NWC:gagesII', 'STAID')
+    streamflow_hucs = get_feature(args.geoserver, 'NWC:huc12_se_basins_v2_local', 'huc12')
+    
+    context = {
+               'waterbudget_hucs' : waterbudget_hucs,
+               'streamflow_gage_ids' : streamflow_gage_ids,
+               'streamflow_hucs' : streamflow_hucs
+               }
+
+    sitemap_file.write(jsontemplate.expand(template.read(), context))
+    
+    template.close()
+    sitemap_file.close()
