074feb077a0115e71c00f3f980f387831709e896
Mary Bucknell <mbucknell@usgs.gov>
2015-08-06 11:04:19 -0500

    Using jinja2 templates rather than jsontemplate. Added code to generate sitemap entries for the data-discovery project details and data details page. Added a README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# National Water Census (NWC) Portal sitemap generator
+
+This repository houses code that is used to generate a sitemap for the NWC portal website.
+
+## Building
+1. Create a virtualenv. The code has been tested using python 2.7.
+2. Install the requirements in the virtualenv by executing ```pip install -r requirements.txt``` while in the virtualenv
+
+## Running generate_sitemap
+The generation script can be run with no arguments  but to see what arguments can be used run the script as follows:
+```
+python generate_sitemap --help
+```
--- generate_sitemap.py
+++ generate_sitemap.py
@@ -1,6 +1,6 @@
 import argparse
 
-import jsontemplate
+from jinja2 import Template
 import requests
 
 def get_feature(geoserver_endpoint, layer, attribute):
@@ -15,34 +15,55 @@ def get_feature(geoserver_endpoint, layer, attribute):
     response.raise_for_status()
     return response.json()
 
+def get_sciencebase_data(sciencebase_endpoint, browse_category):
+    response = requests.get(sciencebase_endpoint + 'catalog/items',
+                            {'facetTermLevelLimit' : 'false',
+                             'q' : '',
+                             'community' : 'National Water Census',
+                             'filter0' : 'browseCategory=' + browse_category,
+                             'format' : 'json'
+                             })
+    response.raise_for_status()
+    return response.json()
+
+
 if __name__=="__main__":
     
     parser = argparse.ArgumentParser(description='Generate sitemap.xml for NWC')
-    parser.add_argument('--geoserver', help='Geoserver to use to retrieve HUCs and gages', 
+    parser.add_argument('--geoserver', 
+                        help='Geoserver to use to retrieve HUCs and gages. Defaults to http://cida.usgs.gov/nwc/geoserver/', 
                         default='http://cida.usgs.gov/nwc/geoserver/')
-    parser.add_argument('--root_url', help='applications root url',
+    parser.add_argument('--sciencebase_url', 
+                        help='URL where the data discovery tool gets its project information. Defaults to https://www.sciencebase.gov/',
+                        default='https://www.sciencebase.gov/')
+    parser.add_argument('--root_url', 
+                        help='The application\'s root url. Defaults to http://cida.usgs.gov/nwc/',
                         default='http://cida.usgs.gov/nwc/')
-    parser.add_argument('--destination_dir', help='Destination directory for the sitemap.xml file',
+    parser.add_argument('--destination_dir', 
+                        help='Destination directory for the sitemap.xml file. Defaults to the directory where the script is run.',
                         default='')
     args = parser.parse_args()
     
-    template = open('sitemap.template', 'r');
+    template_file = open('sitemap_template.xml', 'r');
     sitemap_file = open('%ssitemap.xml' % args.destination_dir, 'w');
+    
+    context = {'root_url' : args.root_url}
     
     print 'Retrieving HUCs and gage IDs from %s' % args.geoserver
     
-    waterbudget_hucs = get_feature(args.geoserver, 'NHDPlusHUCs:nationalwbdsnapshot', 'huc_12')    
-    streamflow_gage_ids = get_feature(args.geoserver, 'NWC:gagesII', 'STAID')
-    streamflow_hucs = get_feature(args.geoserver, 'NWC:huc12_se_basins_v2_local', 'huc12')
+    context['waterbudget_hucs'] = get_feature(args.geoserver, 'NHDPlusHUCs:nationalwbdsnapshot', 'huc_12')    
+    context['streamflow_gage_ids'] = get_feature(args.geoserver, 'NWC:gagesII', 'STAID')
+    context['streamflow_hucs'] = get_feature(args.geoserver, 'NWC:huc12_se_basins_v2_local', 'huc12')
     
-    context = {
-               'root_url' : args.root_url,
-               'waterbudget_hucs' : waterbudget_hucs,
-               'streamflow_gage_ids' : streamflow_gage_ids,
-               'streamflow_hucs' : streamflow_hucs
-               }
-
-    sitemap_file.write(jsontemplate.expand(template.read(), context))
+    print 'Retrieving projects and dataset ids from %s' % args.sciencebase_url 
+            
+    context['projects'] = get_sciencebase_data(args.sciencebase_url, 'Project')['items']
+    context['datasets'] = get_sciencebase_data(args.sciencebase_url, 'Data')['items']
     
-    template.close()
+    print 'Creating sitemap file at %s' % sitemap_file.name
+    
+    template = Template(template_file.read())
+    sitemap_file.write(template.render(context))
+    
+    template_file.close()
     sitemap_file.close()
