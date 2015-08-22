e48ace35046af28c4572f6060f84545afc106fc4
Carl Schroedl <cschroedl@usgs.gov>
2015-08-21 19:16:43 -0500

    Revert "Temporarily reducing precision of time format to facilitate easier comparision between refactored and original implementation."
    This reverts commit 799af3c44c2f307f0210133132ffdbc4f7b60813.

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -197,7 +197,7 @@ if __name__=="__main__":
     
     context = {
                'root_url' : args.root_url,
-               'last_modified' : datetime.datetime.today().strftime("%d/%m/%y")
+               'last_modified' : datetime.datetime.today().isoformat()
                }
     data = get_nwc_data(geoserver, sciencebase)
 
