2c13e5062581fb06ad7ffe37d9ea324dd4e4cd10
Carl Schroedl <cschroedl@usgs.gov>
2015-09-03 16:35:29 -0500

    Checkpoint, untested. Adding new scripts for running generation tasks individually with different output dirs.

--- src/main/python/generate_all.sh
+++ src/main/python/generate_all.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-PREFIX="generate_all.sh:"
-echo "$PREFIX Creating virtualenv for project"
-virtualenv --no-site-packages --python=python2.7 env
-echo "$PREFIX Installing project's python dependencies"
-. env/bin/activate
-pip install -r requirements.txt
+./setup.sh
 python generate_all.py --destination_dir=$1 --geoserver="$2"
---
+++ src/main/python/generate_sitemap.sh
@@ -0,0 +1,4 @@
+
+#!/bin/bash
+./setup.sh
+python generate_sitemap.py --destination_dir=$1 --geoserver="$2"
