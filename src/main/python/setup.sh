eccad4f8eaf2a85112fa8f615fa80c995dce8512
Mary Bucknell <mbucknell@usgs.gov>
2016-05-27 09:44:49 -0500

    Use areasqkm for drainage area in huc12_se_basins_v2.
    Updated the setup.sh script to detect when VIRTUALENV is not set and to then use the system virtualenv

--- src/main/python/setup.sh
+++ src/main/python/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -z "$VIRTUALENV" ]; then
+    export VIRTUALENV=virtualenv
+fi
 $VIRTUALENV --no-site-packages --python=python2.7 env
 . env/bin/activate
 pip install -r requirements.txt
